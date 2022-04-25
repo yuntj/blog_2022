22ea116e441aba14b491c9cc10fe2f936dff53fa
yuntj <416197221@qq.com>
2022-04-25 20:29:01 +0800

    update frontend environment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 
-function App() {
-  return (
-    <div className="App">
-     app component
-    </div>
-  );
-}
+import { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Login from './pages/Login'
 
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        {/* 应用头部 */}
+        <Header />
+
+        {/* 主体页面 */}
+        <Switch>
+          <Route path='/' component={Home} exact />
+          <Route path='/login' component={Login} />
+        </Switch>
+      </div>
+    )
+  }
+}
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            {/* 预留多个SPA,多个根组件入口 */}
+            <Route path="/" component={App} />
+        </Switch>
+    </BrowserRouter>
+    ,
+    document.getElementById('root')
 );
-
